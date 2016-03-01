f40c615b125a8204ff3d3f1a20c8a3d1683959c3
Teisia Park <teisia@gmail.com>
2016-03-01 12:00:54 -0700

    wire up angular routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('pirates', ['ngRoute'])
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/pirates.html',
+      controller: 'PiratesController'
+    })
+});
